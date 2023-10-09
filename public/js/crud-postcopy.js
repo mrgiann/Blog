81705d54347bc3d5a82df9c0609c606a7dec9e12
mrgiann <renzo2008roggia@gmail.com>
2023-10-08 21:31:56 -0300

    Implementacion de editar publicaciones (sin funcionalidad por el momento)

--- public/js/crud-postcopy.js
+++ public/js/crud-postcopy.js
@@ -33,16 +33,50 @@ export const mostrarDatosPublicaciones = (publicaciones, elemento) => {
     let registros = '';
     publicaciones.forEach(pub => {
         registros += `
+
+        <div id="cuadro-editar">
             <section class="editor d-flex">
                 <img src="${pub.url_imagen}" class="rounded" height="80" width="80">
                 <div class="contenidoeditor d-flex justify-content-between">
                     <h4>${pub.titulo}</h4>
                     <p><b>Fecha:</b> ${pub.fecha_publicacion}</p>
                     <p><b>Publicado por:</b> ${pub.firma_autor}</p>
+                    <button id="editarBtn" class="btn btn-sm btn-primary">Editar</button>
+                    <button id="eliminarBtn" class="btn btn-sm btn-primary">Eliminar</button>
                 </div>
             </section>
-        `
+            <div id="formulario-editar">
+            <div class="row" id="formulario-editar">
+                    <form action="#" id="form-editar">
+                        <!-- Agrega un campo oculto para almacenar el ID de la publicación que se está editando -->
+                        <input type="hidden" id="post-id-editar" value="${pub.id}">
+                        
+                        <div class="mb-3">
+                            <input type="text" id="titulo-editar" placeholder="Título" class="form-control" value="${pub.titulo}">
+                        </div>
+                        <div class="form-floating mb-3">
+                            <textarea class="form-control" id="detalle-editar" style="height: 100px">${pub.detalle}</textarea>
+                            <label>Detalle:</label>
+                        </div>
+                        <div class="mb-3">
+                            <input type="text" id="url_imagen-editar" placeholder="URL de la imagen" class="form-control" value="${pub.url_imagen}">
+                        </div>
+                        <div class="mb-3">
+                            <input type="date" id="fecha_publicacion-editar" class="form-control" value="${pub.fecha_publicacion}">
+                        </div>
+                        <div class="mb-3">
+                            <input type="time" id="hora_manual-editar" placeholder="Hora" class="form-control" value="${pub.hora_manual}">
+                        </div>
+                        <div class="mb-3">
+                            <input type="text" id="firma_autor-editar" placeholder="Firma del autor" class="form-control" value="${pub.firma_autor}">
+                        </div>
+                        <button type="submit" id="guardar-edicion-btn" class="btn btn-sm btn-primary">Guardar Edición</button>
+                    </form>
+                </div>
+            </div>
+            </div>
+        `;
     });
 
-    elemento.innerHTML = registros
+    elemento.innerHTML = registros;
 }
